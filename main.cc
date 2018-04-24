c63e8f9f8929276d7bce46bcfe1bb7308a4a74bb
tsqa <tttsqa@gmail.com>
2018-04-24 00:54:40 -0500

    matrix added strlen missing

--- main.cc
+++ main.cc
@@ -7,10 +7,10 @@
 #include <stdint.h>
 #include <sstream>
 
-#define MATRIX_WIDTH 4
-#define MATRIX_HEIGHT 3
-#define BOX_WIDTH 15
-#define MATRIX_NAME_STRING "Test Matrix"
+#define MATRIX_WIDTH 3
+#define MATRIX_HEIGHT 5
+#define BOX_WIDTH 30
+#define MATRIX_NAME_STRING "Binary File Contents"
 
 using namespace std;
 
@@ -28,32 +28,90 @@ uint64_t numRecords;
 uint8_t strLength;
 char stringBuffer[25];
 
-ifstream input ("cs3377.bin", ios::binary);
-input.read(reinterpret_cast<char *>(&magic), sizeof(magic)); // directly load bytes into uint32_t
 
-	cout << "magicNum is: ";
-	cout << hex << magic;
-input.read(reinterpret_cast<char *>(&versionNumber), sizeof(versionNumber));	
-	cout << "versionNum is: ";
-	cout << hex << versionNumber;
-input.read(reinterpret_cast<char *>(&numRecords), sizeof(numRecords));
-	cout << "numRecords is: ";
-	cout << hex << numRecords;
-input.read(reinterpret_cast<char *>(&strLength), sizeof(strLength));
-	cout << "strLength is: ";
-	cout << hex << strLength;
-input.read(reinterpret_cast<char *>(&stringBuffer[0]), sizeof(stringBuffer[0]));
-	cout << "stringBuffer is: ";
-	cout << hex << stringBuffer;
+  const char 		*rowTitles[] = {"R0", "R1", "R2", "R3", "R4", "R5"};
+  const char 		*columnTitles[] = {"C0", "C1", "C2", "C3", "C4", "C5"};
+  int		boxWidths[] = {BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH};
+  int		boxTypes[] = {vMIXED, vMIXED, vMIXED, vMIXED,  vMIXED,  vMIXED};
 
+  /*
+   * Initialize the Cdk screen.
+   *
+   * Make sure the putty terminal is large enough
+   */
+  window = initscr();
+  cdkscreen = initCDKScreen(window);
+
+  /* Start CDK Colors */
+  initCDKColor();
+
+  /*
+   * Create the matrix.  Need to manually cast (const char**) to (char **)
+  */
+  myMatrix = newCDKMatrix(cdkscreen, CENTER, CENTER, MATRIX_HEIGHT, MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_WIDTH,
+			  MATRIX_NAME_STRING, (char **) rowTitles, (char **) columnTitles, boxWidths,
+				     boxTypes, 1, 1, ' ', ROW, true, true, false);
+
+  if (myMatrix ==NULL)
+    {
+      printf("Error creating Matrix\n");
+      _exit(1);
+    }
 stringstream ss;
-ss << hex << magic;
-string str = ss.str();
-	cout << "\n magicNum is: ";
-	cout << str;
+string str;
 
-	cout << " done";
-	unsigned char ax;
-  	cin >> ax;
+ifstream input ("cs3377.bin", ios::binary);
+
+input.read(reinterpret_cast<char *>(&magic), sizeof(magic)); // directly load bytes into uint32_t
+ss << "Magic: " << hex << magic;
+str = ss.str();
+setCDKMatrixCell(myMatrix, 1, 1, str.c_str());
+ss.str("");
+
+input.read(reinterpret_cast<char *>(&versionNumber), sizeof(versionNumber));	
+ss << "Version: " << hex << versionNumber;
+str = ss.str();
+setCDKMatrixCell(myMatrix, 1, 2, str.c_str());
+ss.str("");
+
+input.read(reinterpret_cast<char *>(&numRecords), sizeof(numRecords));
+ss << "NumRecords: " << hex << numRecords;
+str = ss.str();
+setCDKMatrixCell(myMatrix, 1, 3, str.c_str());
+ss.str("");
+
+
+string temp = "";
+int temp2 = 0;
+for(int i = 1; i <= 4; i++){
+input.read(reinterpret_cast<char *>(&strLength), sizeof(strLength));
+input.read(reinterpret_cast<char *>(&stringBuffer), sizeof(stringBuffer));
+//cout << "stringBuffer is: ";
+//cout << hex << stringBuffer << endl;
+string s(stringBuffer); 
+temp2 = strlen(s.c_str());
+cout << "the string is: " << s << " the length of the string is: " << temp2 << endl;
+//temp = to_string(temp2);
+//setCDKMatrixCell(myMatrix, i, 2, temp.c_str());
+setCDKMatrixCell(myMatrix, i+1, 2, stringBuffer);
+}
+
+input.close();
+
+cout << " done";
+unsigned char ax;
+cin >> ax;
+
+//drawCDKMatrix(myMatrix, true);
+//setCDKMatrixCell(myMatrix, 2, 2, "test");
+drawCDKMatrix(myMatrix, true);    /* required  */
+
+  /* So we can see results, pause until a key is pressed. */
+  unsigned char x;
+  cin >> x;
+
+  // Cleanup screen
+  endCDK();
+
 
 }
